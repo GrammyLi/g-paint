60b438ebfe359ba0a90b326b4eb9d97c96de4c0d
魔法师 <grammyli@outlook.com>
2022-03-18 13:01:45 +0800

    add main.js

--- demo/element/squarePen.js
+++ demo/element/squarePen.js
@@ -1,33 +1,37 @@
-class CirclePen extends G {
-  constructor(x, y, ctx, fillColor, edgeColor, penSize) {
+class SquarePen extends G {
+  constructor(x, y, ctx, fillColor, edgeColor) {
     super();
     this.x = x
     this.y = y
     this.ctx = ctx;
     this.fillColor = fillColor || this.ctx.fillStyle
     this.edgeColor = edgeColor || this.ctx.strokeStyle
-    this.penSize = penSize || this.ctx.lineWidth
   }
   updateContextStyle() {
     this._fillStyle = this.ctx.fillStyle
     this._strokeStyle = this.ctx.strokeStyle
-    this._lineWidth = this.ctx.lineWidth
     this.ctx.fillStyle = this.fillColor
     this.ctx.strokeStyle = this.edgeColor
-    this.ctx.lineWidth = this.penSize
   }
   resetContextStyle() {
     this.ctx.fillStyle = this._fillStyle
     this.ctx.strokeStyle = this._strokeStyle
-    this.ctx.lineWidth = this._lineWidth
   }
   draw() {
     const {x, y, ctx} = this
-    const r = this.penSize
-    this.updateContextStyle()
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fill();
+    // let s = this.penSize / 2;
+    let x1 = x - s;
+    let y1 = y - s;
+   
+    let x2 = x + s;
+    let y2 = y + s;
+   
+    const w = x2 - x1;
+    const h = y2 - y1;
+     
+      ctx.fillRect(x1, y1, w, h);
+      ctx.fill();
+   
     this.resetContextStyle()
   }
 }
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const __main = () => {
   const [w, h] = [700, 700];
-  const selector = ".g-canvas";
+  const selector = '.g-canvas'
   Paint.new({ selector, w, h });
 };
 
